8df46821593936011a47956a13727dd9ac0e90e4
lefsampaio <leticia.cosmetic2011@gmail.com>
2019-08-21 17:47:48 -0300

    imagem background/ css e algumas identaçoes no html e no cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -7,10 +7,9 @@ window.cipher = {
     for (let i = 0; i < textToEncode.length; i++) { //Passa por cada letra do input
       //Identifica qual letra do alfabeto
       //posicionando-a dentro do código ASCII
-      //Faz o deslocamento mantendo dentro do alfabeto (26 letras)
-      let letter = (textToEncode.charCodeAt(i) - 65 + parseInt(numbToMove)) % 26 + 65;
-      //Convertendo o valor unicode ao caractere correspondente.
-      result += String.fromCharCode(letter);
+      let letter = (textToEncode.charCodeAt(i) - 65 + parseInt(numbToMove)) % 26 + 65; //Faz o deslocamento mantendo dentro do alfabeto (26 letras)
+
+      result += String.fromCharCode(letter); //Convertendo o valor unicode ao caractere correspondente.
     }
     return result;
   },
@@ -18,9 +17,9 @@ window.cipher = {
   decode: (textToDecode, numbToMove) => {
     let result = "";
     for (let i = 0; i < textToDecode.length; i++) {
-      //coloca a letra dentro do alfabeto 
+
       let letter = (textToDecode.charCodeAt(i) + 65 - parseInt(numbToMove)) % 26 + 65;
-      //Convertendo o valor unicode ao caractere correspondente.     
+
       result += String.fromCharCode(letter);
     }
     return result;
