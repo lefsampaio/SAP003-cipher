3199432fcc2a4c2807c7f6cb3b0bf604d584678a
lefsampaio <leticia.cosmetic2011@gmail.com>
2019-08-22 17:14:16 -0300

    hacker edition 1.0

--- src/cipher.js
+++ src/cipher.js
@@ -2,25 +2,51 @@ window.cipher = {
 
   //funcao de cifrar e de decifrar
 
-  encode: (textToEncode, numbToMove) => {
+  encode: (numberMove, textToEncode) => {
     let result = "";
-    for (let i = 0; i < textToEncode.length; i++) { //Passa por cada letra do input
-      //Identifica qual letra do alfabeto
-      //posicionando-a dentro do código ASCII
-      let letter = (textToEncode.charCodeAt(i) - 65 + parseInt(numbToMove)) % 26 + 65; //Faz o deslocamento mantendo dentro do alfabeto (26 letras)
+    let letter = "";
 
-      result += String.fromCharCode(letter); //Convertendo o valor unicode ao caractere correspondente.
+    for (let i = 0; i < textToEncode.length; i++) { //Passa por cada letra do input
+
+      if (textToEncode.charCodeAt(i) >= 97 && textToEncode.charCodeAt(i) <= 122) {
+        letter = (textToEncode.charCodeAt(i) - 97 + parseInt(numberMove)) % 26 + 97;
+
+        result += String.fromCharCode(letter); //Convertendo o valor unicode ao caractere correspondente.
+
+      } else if (textToEncode.charCodeAt(i) >= 65 && textToEncode.charCodeAt(i) <= 90) {
+
+        letter = (textToEncode.charCodeAt(i) - 65 + parseInt(numberMove)) % 26 + 65;
+        result += String.fromCharCode(letter);
+      }
+      else {
+
+        result += textToEncode[i];
+      }
+
     }
     return result;
   },
 
-  decode: (textToDecode, numbToMove) => {
+  decode: (numberMove, textToDecode) => {
     let result = "";
+    let letter = "";
+
     for (let i = 0; i < textToDecode.length; i++) {
+      if (textToDecode.charCodeAt(i) >= 97 && textToDecode.charCodeAt(i) <= 122) {
 
-      let letter = (textToDecode.charCodeAt(i) + 65 - parseInt(numbToMove)) % 26 + 65;
+        letter = (textToDecode.charCodeAt(i) + 97 - parseInt(numberMove)) % 26 + 97;
 
-      result += String.fromCharCode(letter);
+        result += String.fromCharCode(letter);
+
+      } else if (textToDecode.charCodeAt(i) >= 65 && textToDecode.charCodeAt(i) <= 90) {
+
+        letter = (textToDecode.charCodeAt(i) + 65 - parseInt(numberMove)) % 26 + 65;
+
+        result += String.fromCharCode(letter);
+      }
+      else {
+        result += textToDecode[i];
+      }
     }
     return result;
   }
--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
-//Variaveis dos botoes
-const btnCifrar = document.getElementById("btnCifrar");
-const btnDecifrar = document.getElementById("btnDecifrar");
+window.onload = () => {
+  //Variaveis dos botoes
+  const btnCifrar = document.getElementById("btnCifrar");
+  const btnDecifrar = document.getElementById("btnDecifrar");
 
-//Evento click CIFRAR
-btnCifrar.addEventListener("click", () => {
-  let textToEncode = document.getElementById("messageOne").value.toUpperCase();
+  //Evento click CIFRAR
+  btnCifrar.addEventListener("click", () => {
+    let textToEncode = document.getElementById("mensagem1").value;
 
-  let numbToMove = document.getElementById("numb").value;
+    let numberMove = document.getElementById("number").value;
 
-  let encodedText = window.cipher.encode(textToEncode, numbToMove);
+    let encodedText = window.cipher.encode(numberMove, textToEncode);
 
-  document.getElementById("messageTwo").value = encodedText;
+    document.getElementById("mensagem2").value = encodedText;
 
-});
-//Evento Click DECIFRAR
-btnDecifrar.addEventListener("click", () => {
+  });
+  //Evento Click DECIFRAR
+  btnDecifrar.addEventListener("click", () => {
 
-  let textToDecode = document.getElementById("messageOne").value.toUpperCase();
+    let textToDecode = document.getElementById("mensagem1").value;
 
-  let numbToMove = document.getElementById("numb").value;
+    let numberMove = document.getElementById("number").value;
 
-  let decodedText = window.cipher.decode(textToDecode, numbToMove);
+    let decodedText = window.cipher.decode(numberMove, textToDecode);
 
-  document.getElementById("messageTwo").value = decodedText;
+    document.getElementById("mensagem2").value = decodedText;
 
-});
+  });
 
+};
